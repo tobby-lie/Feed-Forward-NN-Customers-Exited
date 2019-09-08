1856772f9d5c54b245b8792af60901bf25ec9eff
Tobby Lie <tobbylie@gmail.com>
2019-09-08 01:14:21 -0600

    worked on grad code

--- PA1_Grad.py
+++ PA1_Grad.py
@@ -4,6 +4,8 @@
 # Last modified: 9/4/19 @ 8:18PM
 
 from sklearn import preprocessing
+from random import random
+from random import seed
 import math
 import numpy as np
 import csv
@@ -201,115 +203,205 @@ recalls = []
 f1_scores = []
 ######################################################################################################################
 
+learning_rate = 0.5
+for index, label in enumerate(labels):
+	input_layer = training_examples_scaled[index]
+	weights_1 = np.random.rand(len(input_layer), 3)
+	zVal_1 = np.dot(input_layer, weights_1)
+
+	activation_1 = []
+	for zVal in zVal_1:
+		activation_1.append(sigmoid(zVal))
+
+	weights_2 = np.random.rand(len(activation_1), 1)
+
+	output_layer = sigmoid(np.dot(activation_1, weights_2))
+
+	if label == 1:
+		loss = -(math.log(output_layer))
+	elif label == 0:
+		loss = -(math.log(1-output_layer))
+
+	the_label = float(label)
+	error_last = (float(the_label) - float(output_layer))*(float(output_layer) 
+		* (1.0 - float(output_layer)))
+
+	error_hidden = np.zeros(shape=(1,3))
+	for index, activation in enumerate(activation_1):
+		error_hidden[0][index] = (activation*weights_2[index][0])
+
+	# between input and hidden layer
+	delta_1 = np.zeros((len(input_layer), 3))
+
+	activation_1 = np.array(activation_1)
+	delta_1 = delta_1+(activation_1.transpose()*error_hidden)
+
+	delta_2 = np.zeros((len(activation_1), 1))
+	output_layer = np.array(output_layer)
+	delta_2 = delta_2+(output_layer.transpose()*error_last)
+
+	input_layer_mult = []
+	input_layer_mult.append(input_layer)
+	input_layer_mult = np.array(input_layer_mult)
+
+	temp = np.dot(error_hidden.transpose(), input_layer_mult)
+	temp = np.dot(learning_rate, temp)
+	weights_1 = np.add(temp.transpose(), weights_1)
+
+####### trying to calculate weight = weight + learning_rate * error * input from
+######https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/
+	print(activation_1.shape)
+
+	temp = np.dot(error_last, activation_1)
+	temp = np.dot(learning_rate, temp)
+	weights_2 = np.add(temp, weights_2)
+
+	print(weights_2)
+
+
+
+######################################################################################################################
+'''
 
 current_training_example = 0
-
-# Create random weights for each perceptron in hidden layer 1
-# Meaning 4 (perceptrons) x 8 (inputs) = 32 weights total
-# And 8 weights connecting to each perceptron in the hidden layer 1
-
-# list to hold the 4 perceptrons of hidden layer 1
-hidden_layer1_perceptrons = []
-# Run for loop 4 times, once for every perceptron in hidden layer 1
-for x in range(0, 4):
-	# randomly create 8 weights for each perceptron connecting 
-	# the given perceptron to each input with a weight
-	random_weights = np.random.rand(8, 1)
-	hidden_layer1_perceptrons.append(random_weights)
-
-hidden_layer1_perceptrons = np.array(hidden_layer1_perceptrons)
-
-# activation inputs for hidden layer 1
-activation_inputs_1 = []
-for indx, hidden_layer1_perceptron in enumerate(hidden_layer1_perceptrons):
-	temp =[]
-	temp.append(training_examples_scaled[current_training_example])
-	temp = np.array(temp)
-	activation_input = np.dot(temp,hidden_layer1_perceptron)
-	activation_inputs_1.append(activation_input[0])
-
-activation_inputs_1 = np.array(activation_inputs_1)
-
-# Outputs for each perceptron in hidden layer 1
-# after sigmoid has been applied
-activation_outputs_1 = []
-for activation_input in activation_inputs_1:
-	activation_output = sigmoid(activation_input)
-	activation_output_temp = []
-	activation_output_temp.append(activation_output)
-	activation_outputs_1.append(activation_output_temp)
-
-activation_outputs_1 = np.array(activation_outputs_1)
-activation_outputs_1 = np.transpose(activation_outputs_1)
-
-######################################################################################################################
-
-# Hidden layer 2
-
-# list to hold the 4 perceptrons of hidden layer 1
-hidden_layer2_perceptrons = []
-# Run for loop 5 times, once for every perceptron in hidden layer 2
-for x in range(0, 5):
-	# randomly create 4 weights for each perceptron connecting 
-	# the given perceptron to each input with a weight
-	random_weights = np.random.rand(4, 1)
-	hidden_layer2_perceptrons.append(random_weights)
-
-hidden_layer2_perceptrons = np.array(hidden_layer2_perceptrons)
-
-#print(hidden_layer2_perceptrons)
-# activation inputs for hidden layer 1
-activation_inputs_2 = []
-for indx, hidden_layer2_perceptron in enumerate(hidden_layer2_perceptrons):
-	activation_input = np.dot(activation_outputs_1,hidden_layer2_perceptron)
-	activation_inputs_2.append(activation_input[0])
-
-activation_inputs_2 = np.array(activation_inputs_2)
-
-# Outputs for each perceptron in hidden layer 1
-# after sigmoid has been applied
-activation_outputs_2 = []
-for activation_input in activation_inputs_2:
-	activation_output = sigmoid(activation_input)
-	activation_output_temp = []
-	activation_output_temp.append(activation_output)
-	activation_outputs_2.append(activation_output_temp)
-
-activation_outputs_2 = np.array(activation_outputs_2)
-activation_outputs_2 = np.transpose(activation_outputs_2)
-
-######################################################################################################################
-# Ouput Layer
-
-# Get random weights from hidden layer to output perceptron
-random_weights = np.random.rand(5,1)
-output_layer_perceptrons = random_weights[:]
-
-output_layer_perceptrons = np.array(output_layer_perceptrons)
-
-activation_input_out = np.dot(activation_outputs_2, output_layer_perceptrons)
-activation_output_out = sigmoid(activation_input_out)
-
-print(activation_output_out)
-print(labels[0][0])
-
-######################################################################################################################
-# Loss function - binary cross entropy loss function
-
-# If prediction is near correct for label then
-# the loss will be low
-# Otherwise if the prediction is far from correct 
-# based on the label, then loss will be high
-# Cost goes to infinity if label = 0 and prediction approaches 1
-# Cost goes to infinity if label = 1 and prediction approaches 0
-if labels[current_training_example][0] == 1:
-	loss = -(math.log(activation_output_out))
-elif labels[current_training_example][0] == 0:
-	loss = -(math.log(1-activation_output_out))
-
-print(loss)
+learning_rate = 0.5
+true_positives = 0
+true_negatives = 0
+false_positives = 0
+false_negatives = 0
+m = len(labels)
 
 
+	# Create random weights for each perceptron in hidden layer 1
+	# Meaning 4 (perceptrons) x 8 (inputs) = 32 weights total
+	# And 8 weights connecting to each perceptron in the hidden layer 1
+
+for index, training_example_scaled in enumerate(training_examples_scaled):
+	# list to hold the 4 perceptrons of hidden layer 1
+	hidden_layer1_perceptrons = []
+	# Run for loop 4 times, once for every perceptron in hidden layer 1
+	for x in range(0, 4):
+		# randomly create 8 weights for each perceptron connecting 
+		# the given perceptron to each input with a weight
+		random_weights = np.random.rand(8, 1)
+		hidden_layer1_perceptrons.append(random_weights)
+
+	hidden_layer1_perceptrons = np.array(hidden_layer1_perceptrons)
+
+	# activation inputs for hidden layer 1
+	activation_inputs_1 = []
+	for indx, hidden_layer1_perceptron in enumerate(hidden_layer1_perceptrons):
+		temp =[]
+		temp.append(training_examples_scaled[current_training_example])
+		temp = np.array(temp)
+		activation_input = np.dot(temp,hidden_layer1_perceptron)
+		activation_inputs_1.append(activation_input[0])
+
+	activation_inputs_1 = np.array(activation_inputs_1)
+
+	# Outputs for each perceptron in hidden layer 1
+	# after sigmoid has been applied
+	activation_outputs_1 = []
+	for activation_input in activation_inputs_1:
+		activation_output = sigmoid(activation_input)
+		activation_output_temp = []
+		activation_output_temp.append(activation_output)
+		activation_outputs_1.append(activation_output_temp)
+
+	activation_outputs_1 = np.array(activation_outputs_1)
+	activation_outputs_1 = np.transpose(activation_outputs_1)
+
+	######################################################################################################################
+
+	# Hidden layer 2
+
+	# list to hold the 4 perceptrons of hidden layer 1
+	hidden_layer2_perceptrons = []
+	# Run for loop 5 times, once for every perceptron in hidden layer 2
+	for x in range(0, 5):
+		# randomly create 4 weights for each perceptron connecting 
+		# the given perceptron to each input with a weight
+		random_weights = np.random.rand(4, 1)
+		hidden_layer2_perceptrons.append(random_weights)
+
+	hidden_layer2_perceptrons = np.array(hidden_layer2_perceptrons)
+
+	#print(hidden_layer2_perceptrons)
+	# activation inputs for hidden layer 1
+	activation_inputs_2 = []
+	for indx, hidden_layer2_perceptron in enumerate(hidden_layer2_perceptrons):
+		activation_input = np.dot(activation_outputs_1,hidden_layer2_perceptron)
+		activation_inputs_2.append(activation_input[0])
+
+	activation_inputs_2 = np.array(activation_inputs_2)
+
+	# Outputs for each perceptron in hidden layer 1
+	# after sigmoid has been applied
+	activation_outputs_2 = []
+	for activation_input in activation_inputs_2:
+		activation_output = sigmoid(activation_input)
+		activation_output_temp = []
+		activation_output_temp.append(activation_output)
+		activation_outputs_2.append(activation_output_temp)
+
+	activation_outputs_2 = np.array(activation_outputs_2)
+	activation_outputs_2 = np.transpose(activation_outputs_2)
+
+	######################################################################################################################
+	# Ouput Layer
+
+	# Get random weights from hidden layer to output perceptron
+	random_weights = np.random.rand(5,1)
+	output_layer_perceptrons = random_weights[:]
+
+	output_layer_perceptrons = np.array(output_layer_perceptrons)
+
+	activation_input_out = np.dot(activation_outputs_2, output_layer_perceptrons)
+	activation_output_out = sigmoid(activation_input_out)
+
+	######################################################################################################################
+	cost = label[current_training_example][0] - activation_output_out
+
+	######################################################################################################################
+
+	print("Activation output: " + str(activation_output_out))
+	print("Label: " + str(labels[index][0]))
+
+	######################################################################################################################
+	# Loss function - binary cross entropy loss function
+
+	# If prediction is near correct for label then
+	# the loss will be low
+	# Otherwise if the prediction is far from correct 
+	# based on the label, then loss will be high
+	# Cost goes to infinity if label = 0 and prediction approaches 1
+	# Cost goes to infinity if label = 1 and prediction approaches 0
+	if labels[index][0] == 1:
+		loss = -(math.log(activation_output_out))
+	elif labels[index][0] == 0:
+		loss = -(math.log(1-activation_output_out))
+
+	print("Loss: " + str(loss))
+
+	# this means predicted 1 and label is 1 meaning true positive
+	if activation_output_out > 0.5 and labels[index][0] == 1:
+		true_positives += 1
+	# predicted 1 and label is 0 meaning false positive
+	elif activation_output_out > 0.5 and labels[index][0] == 0:
+		false_positives += 1
+	elif activation_output_out < 0.5 and labels[index][0] == 1:
+	# predicted 0 and label is 1 meaning false negative
+		false_negatives += 1
+	# predicted 0 and label is 0 meaning true negative
+	elif activation_output_out < 0.5 and labels[index][0] == 0:
+		true_negatives += 1
+
+	accuracy = (true_positives+true_negatives)/(true_positives+true_negatives
+		+false_positives+false_negatives)
+	print("Accuracy: " + str(accuracy))
+#print(labels)
+
+'''
 ######################################################################################################################
 '''
 print(hidden_layer1_perceptrons[1])
